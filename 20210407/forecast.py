ab4ada40ea480770e7b780be034eb2fc0aacddcf
Kaitlin Smith <kes337@cornell.edu>
2021-05-03 11:11:33 -0400

    updated namelist and produced working code for a visul

---
+++ 20210407/forecast.py
@@ -0,0 +1,73 @@
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+import cartopy.crs as crs
+from cartopy.feature import NaturalEarthFeature
+import numpy as np
+from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
+                         cartopy_ylim, latlon_coords, ALL_TIMES)
+
+ncfile = Dataset("wrfout_d01_2021-04-07_00:00:00")
+
+# Lots of code borrowed from the wrf python tutorial
+
+# Get temperature in C
+temp00 = getvar(ncfile, "tc", timeidx=0)
+temp06 = getvar(ncfile, "tc", timeidx=2)
+temp12 = getvar(ncfile, "tc", timeidx=4)
+temp18 = getvar(ncfile, "tc", timeidx=6)
+temp24 = getvar(ncfile, "tc", timeidx=8)
+temps = temp00#, temp06, temp12, temp18, temp24]
+     
+# Get the latitude and longitude points
+lats, lons = latlon_coords(temp00)
+
+# Get the cartopy mapping object
+cart_proj = get_cartopy(temp00)
+
+# fake while loop :P
+hour = 0
+
+for t in temps:
+    # smooth
+    smooth_t = smooth2d(t, 3, cenweight=4)
+
+    # Create a figure
+    fig = plt.figure(figsize=(12,6))
+    # Set the GeoAxes to the projection used by WRF
+    ax = plt.axes(projection=cart_proj)
+
+    # Download and add the states and coastlines
+    states = NaturalEarthFeature(category="cultural", scale="50m",
+        facecolor="none", name="admin_1_states_provinces_shp")
+    ax.add_feature(states, linewidth=.5, edgecolor="black")
+    ax.coastlines('50m', linewidth=0.8)
+
+    # Make the contours for temp
+    plt.contourf(to_np(lons), to_np(lats), to_np(smooth_t), 10,
+        transform=crs.PlateCarree(), cmap=get_cmap("jet"))
+
+    # Add a color bar
+    plt.colorbar(ax=ax, shrink=.98)
+
+    # Set the map bounds
+    ax.set_xlim(cartopy_xlim(smooth_t))
+    ax.set_ylim(cartopy_ylim(smooth_t))
+
+    # Add the gridlines
+    ax.gridlines(color="black", linestyle="dotted")
+    date = "2021-04-07T00:00:00"
+    time = date + "+" + str(hour) + " hours"
+    plt.title("Temperature (C), " + time)
+    
+    strhr = str(hour) if hour > 9 else "0" + str(hour)
+    plt.savefig("d01T" + strhr + ".png")
+    plt.close()
+    hour += 3
+
+#if __name__ == "__main__":
+    # Open the NetCDF file
+ #   ncfile2 = Dataset("wrfout_d01_2021-04-07_00:00:00")
+  #  plot_t(ncfile1, "d1T")
+   # plot_t(ncfile2, "d2T")
+
--- apr7forecast.py
+++ apr7forecast.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
 import cartopy.crs as crs
 from cartopy.feature import NaturalEarthFeature
-
+import numpy as np
 from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
                          cartopy_ylim, latlon_coords, ALL_TIMES)
 
@@ -11,13 +11,13 @@ from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
 
 def plot_t(ncfile, name):
     # Get temperature in C
-    temp00 = getvar(ncfile, "tc")
+    temp00 = getvar(ncfile, "tc", timeidx = 0)
+    temp06 = getvar(ncfile, "tc", timeidx = 2)
     temp12 = getvar(ncfile, "tc", timeidx = 4)
+    temp18 = getvar(ncfile, "tc", timeidx = 6)
     temp24 = getvar(ncfile, "tc", timeidx = 8)
-    temp36 = getvar(ncfile, "tc", timeidx = 12)
-    temp48 = getvar(ncfile, "tc", timeidx = 16)
-    temps = [temp00, temp12, temp24, temp36, temp48]
-    
+    temps = [temp00, temp06, temp12, temp18, temp24]
+     
     # Get the latitude and longitude points
     lats, lons = latlon_coords(temp00)
 
@@ -30,6 +30,7 @@ def plot_t(ncfile, name):
     for t in temps:
         # smooth
         smooth_t = smooth2d(t, 3, cenweight=4)
+        print(np.shape(to_np(smooth_t)))
 
         # Create a figure
         fig = plt.figure(figsize=(12,6))
@@ -61,12 +62,12 @@ def plot_t(ncfile, name):
     
         plt.savefig(name + str(hour) + ".png")
         plt.show()
-        hour += 1
+        hour += 6
 
 if __name__ == "__main__":
     # Open the NetCDF file
-    ncfile1 = Dataset("wrfout_d01_2021-04-07_00_00_00")
-    ncfile2 = Dataset("wrfout_d01_2021-04-07_00_00_00")
+    ncfile1 = Dataset("wrfout_d01_2021-04-07_00:00:00")
+    ncfile2 = Dataset("wrfout_d01_2021-04-07_00:00:00")
     plot_t(ncfile1, "d1T")
     plot_t(ncfile2, "d2T")
 
